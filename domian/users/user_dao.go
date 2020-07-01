ffdd680b3648188c239829b79058caf7ff87f44d
Carlos <krlos1529@gmail.com>
2020-07-01 14:38:57 -0500

    Select Table

--- domian/users/user_dao.go
+++ domian/users/user_dao.go
@@ -10,40 +10,38 @@ import (
 
 const (
 	indexUniqueEmail = "email_UNIQUE"
+	errorNoRows      = "no rows in result set"
 	queryInsertUser  = "INSERT INTO users(first_name,last_name,email,date_create) VALUES(?,?,?,?);"
-)
-
-var (
-	usersDB = make(map[int64]*User)
+	queryGetUser     = "SELECT id,first_name,last_name,email,date_create FROM users WHERE id=?;"
 )
 
 //Obtengo el Id del Usuario
 func (user *User) Get() *errors.RestErr {
-	if err := users_db.Client.Ping(); err != nil {
-		panic(err)
-	}
-
-	result := usersDB[user.Id]
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
-	}
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
-	return nil
-}
-func (user *User) Save() *errors.RestErr {
-	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	stmt, err := users_db.Client.Prepare(queryGetUser)
 	if err != nil {
-
 		return errors.NewInternalServerError(err.Error())
 	}
 	defer stmt.Close()
+	result := stmt.QueryRow(user.Id)
+	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+		if strings.Contains(err.Error(), errorNoRows) {
+			return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+		}
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to get user %d: %s", user.Id, err.Error()))
+	}
 
+	return nil
+}
+
+//Save usuario
+func (user *User) Save() *errors.RestErr {
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+	defer stmt.Close()
 	user.DateCreated = date_utils.GetNowString()
-
 	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
 	if err != nil {
 		if strings.Contains(err.Error(), indexUniqueEmail) {
@@ -53,12 +51,9 @@ func (user *User) Save() *errors.RestErr {
 			fmt.Sprintf("error when trying to save user:%s", err.Error()))
 	}
 	userId, err := insertResult.LastInsertId()
-
 	if err != nil {
 		return errors.NewInternalServerError(fmt.Sprintf("error when trying save user: %s", err.Error()))
 	}
-
 	user.Id = userId
-
 	return nil
 }
