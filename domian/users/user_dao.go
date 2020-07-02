0651fe475b8db57c12b5ef4763acd2d8f8fc6205
Carlos <krlos1529@gmail.com>
2020-07-02 16:02:10 -0500

    search, encriptacion password

--- domian/users/user_dao.go
+++ domian/users/user_dao.go
@@ -2,16 +2,18 @@ package users
 
 import (
 	"bookstore_users-api/datasource/mysql/users_db"
-	"bookstore_users-api/util/date_utils"
 	"bookstore_users-api/util/errors"
 	"bookstore_users-api/util/msql_utils"
+	"fmt"
+	"log"
 )
 
 const (
-	queryInsertUser = "INSERT INTO users(first_name,last_name,email,date_create) VALUES(?,?,?,?);"
-	queryGetUser    = "SELECT id,first_name,last_name,email,date_create FROM users WHERE id=?;"
-	queryUpdateUser = "UPDATE users SET first_name = ?,last_name=?,email=? WHERE id=?;"
-	queryDeleteUser = "DELETE FROM users WHERE id=?;"
+	queryInsertUser       = "INSERT INTO users(first_name,last_name,email,password,status,date_create) VALUES(?,?,?,?,?,?);"
+	queryGetUser          = "SELECT id,first_name,last_name,email,date_create,status FROM users WHERE id=?;"
+	queryUpdateUser       = "UPDATE users SET first_name = ?,last_name=?,email=? WHERE id=?;"
+	queryDeleteUser       = "DELETE FROM users WHERE id=?;"
+	queryFindUserByStatus = "SELECT id,first_name,last_name,email,date_create,status FROM users WHERE status=?;"
 )
 
 //Obtengo el Id del Usuario
@@ -22,7 +24,7 @@ func (user *User) Get() *errors.RestErr {
 	}
 	defer stmt.Close()
 	result := stmt.QueryRow(user.Id)
-	if getErr := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); getErr != nil {
+	if getErr := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated, &user.Status); getErr != nil {
 		return msql_utils.ParseError(getErr)
 	}
 	return nil
@@ -35,8 +37,8 @@ func (user *User) Save() *errors.RestErr {
 		return errors.NewInternalServerError(err.Error())
 	}
 	defer stmt.Close()
-	user.DateCreated = date_utils.GetNowString()
-	insertResult, saveErr := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	//user.DateCreated = date_utils.GetNowDBFormat()
+	insertResult, saveErr := stmt.Exec(user.FirstName, user.LastName, user.Email, user.Password, user.Status, user.DateCreated)
 	if saveErr != nil {
 		return msql_utils.ParseError(saveErr)
 	}
@@ -74,4 +76,38 @@ func (user *User) Delete() *errors.RestErr {
 	}
 
 	return nil
+}
+
+func (user *User) FindByStatus(status string) ([]User, *errors.RestErr) {
+	stmt, err := users_db.Client.Prepare(queryFindUserByStatus)
+	if err != nil {
+		return nil, errors.NewInternalServerError(err.Error())
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query(status)
+
+	if err != nil {
+		return nil, errors.NewInternalServerError(err.Error())
+
+	}
+
+	defer rows.Close()
+	results := make([]User, 0)
+
+	for rows.Next() {
+		var user User
+
+		//SELECT id,first_name,last_name,email,date_create,status
+		if err := rows.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated, &user.Status); err != nil {
+			log.Println("Aqui se callo -")
+			return nil, msql_utils.ParseError(err)
+
+		}
+		results = append(results, user)
+	}
+
+	if len(results) == 0 {
+		return nil, errors.NewNotFoundError(fmt.Sprintf("no user matching status %s", status))
+	}
+	return results, nil
 }
--- services/user_service.go
+++ services/user_service.go
@@ -2,6 +2,8 @@ package services
 
 import (
 	"bookstore_users-api/domian/users"
+	"bookstore_users-api/util/crypto_utils"
+	"bookstore_users-api/util/date_utils"
 	"bookstore_users-api/util/errors"
 )
 
@@ -18,6 +20,9 @@ func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	if err := user.Validate(); err != nil {
 		return nil, err
 	}
+	user.Status = users.StatusActive
+	user.DateCreated = date_utils.GetNowDBFormat()
+	user.Password = crypto_utils.GetMd5(user.Password)
 	if err := user.Save(); err != nil {
 		return nil, err
 	}
@@ -59,5 +64,11 @@ func UpdateUser(isPartial bool, user users.User) (*users.User, *errors.RestErr) 
 func DeleteUser(userId int64) *errors.RestErr {
 	user := &users.User{Id: userId}
 	return user.Delete()
+
+}
+
+func Search(status string) ([]users.User, *errors.RestErr) {
+	dao := &users.User{}
+	return dao.FindByStatus(status)
 
 }
